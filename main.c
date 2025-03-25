ca59bb6e67bd6046385bde0567c009a721437314
Aionmizu <marjolinf@gmail.com>
2025-03-25 10:17:33 +0100

    Thomas la voiture

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <limits.h>
+#include <float.h>
+#include <ncurses.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}

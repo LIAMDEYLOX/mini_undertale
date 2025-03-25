72b1fc0f6ad6b08faefa1402a00d8713c9c184c7
Aionmizu <marjolinf@gmail.com>
2025-03-25 15:26:32 +0100

    Thomas l'avion

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
+#include <assert.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <limits.h>
-#include <float.h>
-#include <ncurses.h>
+#include <allegro5/allegro.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+
+
+#include <allegro5/allegro.h>
+
+int main() {
+    if (!al_init()) {
+        fprintf(stderr, "Failed to initialize Allegro!\n");
+        return -1;
+    }
+
+    ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+    if (!display) {
+        fprintf(stderr, "Failed to create display!\n");
+        return -1;
+    }
+
+    al_clear_to_color(al_map_rgb(50, 50, 50));
+    al_flip_display();
+
+    al_rest(100); // Attend 5 secondes
+    al_destroy_display(display);
     return 0;
 }
